52b6d8e90f8065e34c08b5fedef56ecd02f6a05c
usernamedrew <59914219+ssvjugg@users.noreply.github.com>
2025-07-12 15:23:29 +0300

    Initial commit

---
+++ src/main/java/ru/usernamedrew/kafkahometaskt1/KafkaHometaskT1Application.java
@@ -0,0 +1,13 @@
+package ru.usernamedrew.kafkahometaskt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHometaskT1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaHometaskT1Application.class, args);
+    }
+
+}

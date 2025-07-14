da22b1ad8048be0166cd85a6cb2210aed3698892
Андрей Бурлачко <andrejburlacko@MacBook-Air-Andrej.local>
2025-07-14 03:07:19 +0300

    Analytics added

--- src/main/java/ru/usernamedrew/kafkahometaskt1/controllers/WeatherController.java
+++ src/main/java/ru/usernamedrew/kafkahometaskt1/controllers/WeatherController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.usernamedrew.kafkahometaskt1.dto.WeatherDTO;
+import ru.usernamedrew.kafkahometaskt1.kafka.WeatherConsumer;
 import ru.usernamedrew.kafkahometaskt1.kafka.WeatherProducer;
 
 @RestController
@@ -13,6 +14,8 @@ import ru.usernamedrew.kafkahometaskt1.kafka.WeatherProducer;
 public class WeatherController {
 
     private final WeatherProducer weatherProducer;
+
+    private final WeatherConsumer weatherConsumer;
 
     @PostMapping("send")
     public String send(@RequestBody WeatherDTO weatherDTO) {
@@ -25,6 +28,19 @@ public class WeatherController {
 
     @GetMapping("analytics")
     public String getAnalytics() {
-        return "Empty"; // TODO
+        // Логики не много поэтому оставил в контролере
+        StringBuilder analytics = new StringBuilder();
+
+        analytics.append("Максимальная температура по городам:\n");
+        weatherConsumer.getMaxTemperature()
+                .forEach((city, temperature) ->
+                        analytics.append(String.format("- %s: %.1f°C\n", city, temperature)));
+
+        analytics.append("Минимальная температура по городам:\n");
+        weatherConsumer.getMinTemperature()
+                .forEach((city, temperature) ->
+                        analytics.append(String.format("- %s: %.1f°C\n", city, temperature)));
+
+        return analytics.toString();
     }
 }
--- src/main/java/ru/usernamedrew/kafkahometaskt1/kafka/WeatherConsumer.java
+++ src/main/java/ru/usernamedrew/kafkahometaskt1/kafka/WeatherConsumer.java
@@ -1,19 +1,41 @@
 package ru.usernamedrew.kafkahometaskt1.kafka;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ru.usernamedrew.kafkahometaskt1.dto.WeatherDTO;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 @Slf4j
 public class WeatherConsumer {
+    // Знаю, не лучшее решение хранить так аналитику. Неуспеваю сделать норм :(
+    private final Map<String, Double> maxTemperature = new HashMap<>();
+    private final Map<String, Double> minTemperature = new HashMap<>();
 
     @KafkaListener(topics = "weather")
     public void listen(ConsumerRecord<Long, WeatherDTO> record) {
-        log.info("Received weather message: " + record.value());
-        System.out.println(record.value());
+        WeatherDTO weatherDTO = record.value();
+        log.info("Received weather message: " + weatherDTO.toString());
 
+        updateWeatherAnalytics(weatherDTO);
+    }
+
+    private void updateWeatherAnalytics(WeatherDTO weatherDTO) {
+        maxTemperature.merge(weatherDTO.getCity(), weatherDTO.getTemperature(), Math::max);
+        minTemperature.merge(weatherDTO.getCity(), weatherDTO.getTemperature(), Math::min);
+    }
+
+    public Map<String, Double> getMaxTemperature() {
+        return Collections.unmodifiableMap(maxTemperature);
+    }
+
+    public Map<String, Double> getMinTemperature() {
+        return Collections.unmodifiableMap(minTemperature);
     }
 }
